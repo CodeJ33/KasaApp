4cf05d4533a3a668a7e54603ba7181652a00822a
Ané Julian <julian.ane03@gmail.com>
2024-06-04 18:37:58 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route,  HashRouter } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Error from "./pages/Error/Error";
+import FicheLogement from "./pages/FicheLogement/FicheLogement";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const App = () => {
+  return ( 
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/accueil" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/logement/:id" element = {<FicheLogement />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </HashRouter>
   );
-}
+};
 
 export default App;
